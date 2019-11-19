1577d0c1d827949b19b46599d9c78fbba192a152
lens <lens.huygh@gmail.com>
2019-11-19 23:02:02 +0100

    without atomicity

--- src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -8,10 +8,13 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
@@ -69,6 +72,40 @@ class SampleDataInitializer {
 
 
 interface ReservationRepository extends ReactiveCrudRepository<Reservation, Integer> {
+}
+
+// transactions.. roll back when not succeeded...all was built on threadlocals... in reactive ReactiveTransactions are used
+@Service
+@RequiredArgsConstructor
+class ReservationService{
+    private final ReservationRepository reservationRepository;
+
+    Flux<Reservation> saveAll(String ... names){
+
+        /*
+        normal
+
+        Flux<String> stringFlux = Flux.fromArray(names);
+        Flux<Reservation> map = stringFlux
+                .map(name -> new Reservation(null, name));
+        Flux<Reservation> reservationFlux = map.flatMap(this.reservationRepository::save);
+        return reservationFlux.doOnNext(r -> Assert.isTrue(isValid(r),"the name must have a capital first letter"));*/
+
+
+        // chained
+        return Flux
+                .fromArray(names)
+                .map(name -> new Reservation(null,name))
+                .flatMap(this.reservationRepository::save)
+                .doOnNext(r -> Assert.isTrue(isValid(r),"the name must have a capital first letter"));
+
+    }
+
+
+    // asserts that each name starts with a capital letter
+    private boolean isValid(Reservation reservation){
+        return Character.isUpperCase(reservation.getName().charAt(0));
+    }
 }
 
 @Data
